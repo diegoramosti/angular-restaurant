65f9ac115103797d4471786318255183ce1a1c4b
diego.ramos.iterative <diego.ramos.iterative@outlook.com>
2018-05-17 22:59:11 -0300

    add restaurant-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { HomeComponent } from './home/home.component';
 import { Routes } from "@angular/router";
@@ -7,5 +8,6 @@ export const ROUTES: Routes =
 [
     { path: '', component: HomeComponent},
     { path: 'about', component: AboutComponent},
-    {path: 'restaurants', component: RestaurantsComponent}
+    {path: 'restaurants', component: RestaurantsComponent},
+    {path: 'restaurants/:id', component: RestaurantDetailComponent}
 ]
